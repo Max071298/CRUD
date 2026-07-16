041f5fc97200126529ca29abfa06f6adc2bf3504
MaksRybolt <102859892+Max071298@users.noreply.github.com>
2026-07-17 01:46:58 +0300

    feat: add container for DB (docker compose)

---
+++ src/database/migrations/1784241898920-my-migration.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class MyMigration1784241898920 implements MigrationInterface {
+    name = 'MyMigration1784241898920'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "users" ("userId" uuid NOT NULL DEFAULT uuid_generate_v4(), "login" text NOT NULL, "email" text NOT NULL, "password" text NOT NULL, "age" integer NOT NULL, "description" text NOT NULL, "updated_at" bigint, "deleted_at" bigint, CONSTRAINT "PK_8bf09ba754322ab9c22a215c919" PRIMARY KEY ("userId"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "users"`);
+    }
+
+}
