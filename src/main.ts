720aa0d12590535c0b33797b94f3e0d487d3c983
MaksRybolt <102859892+Max071298@users.noreply.github.com>
2026-07-17 18:28:53 +0300

    feat: implement swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { load } from 'js-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  const openApiYaml = await readFile('api.yml', 'utf8');
+  const documentFactory = (await load(openApiYaml)) as OpenAPIObject;
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
